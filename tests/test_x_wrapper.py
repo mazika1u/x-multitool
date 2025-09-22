1d3252170c7b9ecc32486879dd005e9c404633ab
mazika1u <aaa.me@outlook.jp>
2025-09-23 08:19:43 +0900

    Update test_x_wrapper.py

--- tests/test_x_wrapper.py
+++ tests/test_x_wrapper.py
@@ -1,1 +1,16 @@
+import unittest
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
+from src.x_wrapper import XWrapper
+
+class TestXWrapper(unittest.TestCase):
+    def setUp(self):
+        self.wrapper = XWrapper()
+    
+    def test_guest_token(self):
+        self.assertIsNotNone(self.wrapper.guest_token)
+
+if __name__ == '__main__':
+    unittest.main()
